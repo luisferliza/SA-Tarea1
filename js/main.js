7c843bed9b5279fba6af442662365ce7f9ebdbf3
Luis Lizama <luisferliza@gmail.com>
2020-08-08 10:28:45 -0600

    Token Generado

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 function getSecret() {
-    let url =  `https://api.softwareavanzado.world/index.php?option=token&api=oauth2
-                &grant_type=client_credentials
-                &client_id=sa
-                &client_secret=fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745`
+    let url =  'https://api.softwareavanzado.world/index.php?'
+                + 'option=token'
+                + '&api=oauth2'                
     $.ajax({
-        type: 'GET',
+        type: 'POST',
         url: url,        
+        data : {
+            "grant_type" : "client_credentials",
+            "client_id" : "sa",
+            "client_secret":"fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745"        
+            },
         success: function (data) {
             console.log(data)
         }
